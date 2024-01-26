e898e05b6eda8bcd5957843a4f932df2019ed306
jakelizzI <jakelizzI@gmail.com>
2024-01-26 21:45:01 +0000

    fix: 不要な sleep を削除

--- app/mockApi.ts
+++ app/mockApi.ts
@@ -8,8 +8,6 @@ export type Position = {
 const CENTER = { lat: 35.68131, lng: 139.76716}
 export default async function getCarPosition(): Promise<CarPosition> {
 
-  await new Promise(resolve => setTimeout(resolve, 3000)) // 3秒待つ
-
   const rand = Math.random() / 100;
   const newLat = CENTER.lat - rand;
   const newLng = CENTER.lng - rand;
